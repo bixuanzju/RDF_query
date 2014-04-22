a8240cae0a01f484ab7495a15989df4a34b1d98f
Jeremy Bi <bixuanxbi@gmail.com>
2014-04-22 16:02:41 +0800

    refactor, make use of qplan

--- src/main/scala/Query.scala
+++ src/main/scala/Query.scala
@@ -15,71 +15,69 @@ object Query {
     val sc = new SparkContext(conf)
 
     val queryString = " PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
-                        " PREFIX ub: <http://www.lehigh.edu/hp2/2004/0401/univ-bench.owl#> " +
-                        " SELECT ?X ?Y ?Z" +
-                        " WHERE " +
-                        " { " +
-                        "       ?X rdf:type ub:Student." +
-                        "       ?Y rdf:type ub:Department." +
-                        "       ?X ub:memberOf ?Y." +
-                        "       ?Y ub:subOrganizationOf <http://www.University0.edu>." +
-                        "       ?X ub:emailAddress ?Z"+
-                        " }"
+                      " PREFIX ub: <http://swat.cse.lehigh.edu/onto/univ-bench.owl#> " +
+                      " SELECT ?X ?Y ?Z" +
+                      " WHERE " +
+                      " { " +
+                      "       ?X rdf:type ub:UndergraduateStudent." +
+                      "       ?Y rdf:type ub:Department." +
+                      "       ?X ub:memberOf ?Y." +
+                      "       ?Y ub:subOrganizationOf <http://www.University0.edu>." +
+                      "       ?X ub:emailAddress ?Z"+
+                      " }"
+
+    val mapPath = "/Users/jeremybi/Desktop/new_data/data/mapping/part-r-00000"
+
+    val lookup = sc.textFile(mapPath).
+      map(line => line.split(" ")).
+      map(array => (array(0).toLong, (array(1)))).collect.toMap
 
     val query = new SSEDS(queryString)
-    var joined = sc.parallelize(Array(("", Vector("")))).cache
+    var joined = sc.parallelize(Array((-1L, Vector(-1L)))).cache
 
-    query.splan.foreach(
-      splan => {
-        splan.bgp_index.foreach(
-          index => {
-            val bgp = query.newbgp(index)
+    for (i <- 0 until query.qplan.length) {
+      val plan = query.qplan(i)
 
-            val Regex1 = """<.*#([a-zA-Z]+)>""".r
-            val pred = bgp.bgp_predicate.split("_").map {
-              case Regex1(p) => p
-            }.reduceLeft(_ + "_" + _)
+      for (index <- plan.bgp_index) {
+        val bgp = query.newbgp(index)
 
-            val fileNames = superClass(pred.split("_"))
+        // TODO: drop superClass for the moment
+        val fileName = "ff" + bgp.bgp_predicate_id
 
-            val Regex2 = """\(.*,\((.*),(.*)\)\)$""".r
-            val tuples = fileNames.
-              map(file => sc.textFile("hdfs://localhost:9000/user/jeremybi/" + file)).
-              reduceLeft(_ ++ _).
-              map {
-                case Regex2(p1, p2) =>
-                  if (splan.name == bgp.bgp_var(0))
-                    (p1, p2)
-                  else
-                    (p2, p1)
-                case _ => ("noMatch", "noMatch")}.
-              partitionBy(new HashPartitioner(8))
+        val Regex2 = """\((-?\d+),(-?\d+)\)""".r
 
+        // swap positions for this join
+        val tuples = sc.textFile("hdfs://localhost:9000/user/jeremybi/partitions/" + fileName).
+          map {
+            case Regex2(p1, p2) =>
+              if (plan.name == bgp.bgp_var(0))
+                (p1.toLong, p2.toLong)
+              else
+                (p2.toLong, p1.toLong)
+            case _ => (-1L, -1L)
+          } // TODO: partitionBy
 
-            if (index == 0)
-              joined = tuples.map {
-                case (key, value) => (key, Vector(value))}
-            else
-              joined = joined.join(tuples).mapValues {
-                case (vals, value) => (vals :+ value)}})
-
-        // swap position for next join
-        if (splan.vars(0) == "")
-          joined.collect.foreach {
-            case (key, vals) => println(key)
-          }
-          // println("Total number is " + joined.collect.length)
+        if (index == 0)
+          joined = tuples.map {
+            case (key, value) => (key, Vector(value))}
         else
-          joined = joined map {
-            case (key, vals) => (vals(1), vals updated (1, key))
-          }
+          joined = joined.join(tuples).mapValues {
+            case (vals, value) => (vals :+ value)}
       }
-    )
 
-    // val lines1 = sc.textFile("/Users/jeremybi/Desktop/Q8.txt").collect
-    // val lines2 = sc.textFile("/Users/jeremybi/Desktop/Output.txt").collect
-
-    // lines1.filter(line => !(lines2 contains line)).foreach(println)
+      // output
+      if (i == query.qplan.length - 1)
+        // joined.collect.foreach {
+        //   case (key, vals) => println(lookup(vals(2)))}
+        println("Record number is " + joined.count)
+      else
+        // swap two positions for next join
+        if (plan.vars(0) != -1 && plan.vars(1) != -1) joined = joined
+      else
+        // swap one position for next join
+        joined = joined map {
+          case (key, vals) => (vals(plan.vars(0)), vals updated (plan.vars(0), key))}
+    }
 
     sc.stop()
 
