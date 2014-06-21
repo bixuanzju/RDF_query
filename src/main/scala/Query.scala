7678332e49ec35a20a318fa675b45cc552d0acde
Jeremy Bi <bixuanxbi@gmail.com>
2014-06-21 16:42:36 +0800

    try again

--- src/main/scala/Query.scala
+++ src/main/scala/Query.scala
@@ -1,6 +1,7 @@
 import org.apache.spark
 import org.apache.spark._
 import org.apache.spark.SparkContext._
+import scala.util.control._
 import common._
 
 object Query {
@@ -31,80 +32,93 @@ object Query {
     val _ = new QueryString()
     val query = new SSEDS(QueryString.q(queryNum))
 
+    val outer = new Breaks;
+
     var joined = sc.parallelize(Array(((-1L, -1L), Vector(-1L))))
+    var tuples = sc.parallelize(Array(((-1L, -1L), -1L)))
 
-    for (i <- 0 until query.qplan.length) {
-      val plan = query.qplan(i)
+    outer.breakable {
+      for (i <- 0 until query.qplan.length) {
+        val plan = query.qplan(i)
 
-      for (index <- plan.bgp_index) {
-        val bgp = query.newbgp(index)
+        for (index <- plan.bgp_index) {
+          val bgp = query.newbgp(index)
 
-        val fileName =
-          superClass((if (bgp.bgp_predicate_id == typeHash)
-                        bgp.bgp_object_id
-                      else bgp.bgp_predicate_id).
-                       split("_")).map("ff" + _).
-            filter(filePaths.value contains _)
+          val fileName =
+            superClass((if (bgp.bgp_predicate_id == typeHash)
+                          bgp.bgp_object_id
+                        else bgp.bgp_predicate_id).
+                         split("_")).map("ff" + _).
+              filter(filePaths.value contains _)
 
-        val Regex1 = """\((-?\d+),(-?\d+)\)""".r
-        val Regex2 = """(-?\d+)""".r
+          val Regex1 = """\((-?\d+),(-?\d+)\)""".r
+          val Regex2 = """(-?\d+)""".r
 
-        // swap positions for this join
-        val tuples =
-          if (fileName.isEmpty) sc.parallelize(Array(((-1L, -1L), -1L)))
+          tuples = sc.parallelize(Array(((-1L, -1L), -1L)))
+          // swap positions for this join
+          if (fileName.isEmpty) outer.break
           else
-            fileName.map(name =>
-              sc.textFile("hdfs://192.168.13.200:9000/user/root/partitions/" + name)).
-              reduceLeft(_ ++ _).
-              map {
-                case Regex1(p1, p2) =>
-                  if (plan.name == bgp.bgp_var(0))
-                    ((p1.toLong, -1L), p2.toLong)
-                  else if (plan.name.length == 2)
-                    ((p1.toLong, p2.toLong), -1L)
-                  else ((p2.toLong, -1L), p1.toLong)
-                case Regex2(p) => ((p.toLong, -1L), -1L)
-                case _ => ((-1L, -1L), -1L)
-              }.
-              filter {
-                case (_, obj) =>
-                  if (bgp.bgp_type == "_PO" && bgp.bgp_predicate_id != typeHash)
-                    obj == bgp.bgp_object_id.toLong
-                  else if (bgp.bgp_type == "SP_") obj == bgp.bgp_subject_id.toLong
-                  else true
-              }.partitionBy(new HashPartitioner(3))
+            tuples =
+              fileName.map(name =>
+                sc.textFile("hdfs://192.168.13.200:9000/user/root/partitions/" + name)).
+                reduceLeft(_ ++ _).
+                map {
+                  case Regex1(p1, p2) =>
+                    if (plan.name == bgp.bgp_var(0))
+                      ((p1.toLong, -1L), p2.toLong)
+                    else if (plan.name.length == 2)
+                      ((p1.toLong, p2.toLong), -1L)
+                    else ((p2.toLong, -1L), p1.toLong)
+                  case Regex2(p) => ((p.toLong, -1L), -1L)
+                  case _ => ((-1L, -1L), -1L)
+                }.
+                filter {
+                  case (_, obj) =>
+                    if (bgp.bgp_type == "_PO" && bgp.bgp_predicate_id != typeHash)
+                      obj == bgp.bgp_object_id.toLong
+                    else if (bgp.bgp_type == "SP_") obj == bgp.bgp_subject_id.toLong
+                    else true
+                }.partitionBy(new HashPartitioner(3))
 
 
-        if (index == 0)
-          joined = tuples.map {
-            case (key, value) => (key, Vector(value))}
-        else
-          joined = joined.join(tuples).mapValues {
-            case (vals, value) => (vals :+ value)}
-      }
+          if (index == 0)
+            joined = tuples.map {
+              case (key, value) => (key, Vector(value))}
+          else
+            joined = joined.join(tuples).mapValues {
+              case (vals, value) => (vals :+ value)}
+        }
 
-      // output
-      if (i == query.qplan.length - 1) {
-        // joined.collect.foreach {
-        //   case (key, vals) => println(key._1)}
-        println("Record number is " + joined.count)
-        // joined.collect
-        //stop timing
-        val endTime = System.currentTimeMillis()
-        val totalTime = endTime - startTime;
-        println("Running time is " + totalTime)
-      }
-      else
-        // swap two positions for next join
-        if (plan.vars(0) != -1 && plan.vars(1) != -1)
-        joined = joined map {
-          case (key, vals) => ((vals(plan.vars(0)), vals(plan.vars(1))),
-                                vals updated (plan.vars(0), key._1) updated (plan.vars(1), -1L))
+        // output
+        if (i == query.qplan.length - 1) {
+          // joined.collect.foreach {
+          //   case (key, vals) => println(key._1)}
+          println("Record number is " + joined.count)
+          // joined.collect
+          //stop timing
+          val endTime = System.currentTimeMillis()
+          val totalTime = endTime - startTime;
+          println("Running time is " + totalTime)
         }
         else
-          // swap one position for next join
+          // swap two positions for next join
+          if (plan.vars(0) != -1 && plan.vars(1) != -1)
           joined = joined map {
-            case (key, vals) => ((vals(plan.vars(0)), -1L), vals updated (plan.vars(0), key._1))}
+            case (key, vals) => ((vals(plan.vars(0)), vals(plan.vars(1))),
+                                 vals updated (plan.vars(0), key._1) updated (plan.vars(1), -1L))
+          }
+          else
+            // swap one position for next join
+            joined = joined map {
+              case (key, vals) => ((vals(plan.vars(0)), -1L), vals updated (plan.vars(0), key._1))}
+      }
+    }
+
+    if (tuples.count == 1) {
+      println("Record number is 0")
+      val endTime = System.currentTimeMillis()
+      val totalTime = endTime - startTime;
+      println("Running time is " + totalTime)
     }
 
     sc.stop()
